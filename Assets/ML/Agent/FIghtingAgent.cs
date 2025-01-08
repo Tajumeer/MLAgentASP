6011d656bed97ba6cad1f9a5e438e562fa724db6
tajumeer <becker.sven@hotmail.com>
2025-01-08 21:51:08 +0100

    Added attack function and 5 Million runs

--- Assets/ML/Agent/FIghtingAgent.cs
+++ Assets/ML/Agent/FIghtingAgent.cs
@@ -26,10 +26,11 @@ public class FIghtingAgent : Agent
     private float m_previousDistance;
     private int m_maxHealth;
     private bool m_isBlocking;
+    private bool m_isRewardedForRange = false;
     private Color m_startingColor;
     private Vector2 m_startingPos;
 
- 
+
     public override void Initialize()
     {
         base.Initialize();
@@ -37,12 +38,15 @@ public class FIghtingAgent : Agent
         m_startingPos = this.transform.position;
         m_startingColor = m_sr.color;
         m_maxHealth = m_health;
+
     }
 
     public override void OnEpisodeBegin()
     {
         transform.position = m_startingPos;
         m_rb.linearVelocity = Vector2.zero;
+        m_isRewardedForRange = false;
+        m_opponent.GetComponent<Dummy>().ResetDummy();
     }
     private void Start()
     {
@@ -53,8 +57,12 @@ public class FIghtingAgent : Agent
     public override void CollectObservations(VectorSensor _sensor)
     {
         _sensor.AddObservation(transform.position.x);
-
         _sensor.AddObservation(m_opponent.position.x);
+
+        float distanceToOpponent = Vector2.Distance(transform.position, m_opponent.position);
+        _sensor.AddObservation(distanceToOpponent);
+
+        _sensor.AddObservation(Time.time - m_lastAttackTimer >= m_attackCooldown ? 1f : 0f);
     }
 
     private void FixedUpdate()
@@ -69,17 +77,64 @@ public class FIghtingAgent : Agent
 
         m_rb.linearVelocity = new Vector2(moveX * m_movementSpeed, m_rb.linearVelocityY);
 
+        int attackAction = _actions.DiscreteActions[0];
+
         float distanceToTarget = Vector2.Distance(transform.position, m_opponent.position);
 
         if (distanceToTarget <= m_attackRange)
         {
-            AddReward(1f);
-            EndEpisode();
+            if (!m_isRewardedForRange)
+            {
+                AddReward(1f);
+                m_isRewardedForRange = true;
+            }
         }
         else
         {
+            m_isRewardedForRange = false;
             AddReward(-0.001f);
         }
 
+        if (attackAction == 1)
+        {
+            if (distanceToTarget > m_attackRange)
+            {
+                AddReward(-1f);
+                return;
+            }
+            Attack();
+        }
+
+    }
+
+    private void Attack()
+    {
+
+
+        if (Time.time - m_lastAttackTimer < m_attackCooldown)
+        {
+            AddReward(-.5f);
+            return;
+        }
+
+        m_lastAttackTimer = Time.time;
+
+        Dummy enemy = m_opponent.GetComponent<Dummy>();
+
+        if (enemy != null)
+        {
+            enemy.RecieveDamage(m_attackDamage);
+            AddReward(1f);
+
+            if (enemy.IsDead)
+            {
+                AddReward(5f);
+                EndEpisode();
+            }
+        }
+        else
+        {
+            AddReward(-0.1f);
+        }
     }
 }
--- Assets/Scripts/Dummy.cs
+++ Assets/Scripts/Dummy.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class Dummy : MonoBehaviour
 {
 
-    [SerializeField] private float m_dummyHealth;
-    private float m_maxHealth;
+    [SerializeField] private float m_maxHealth;
+    private float m_dummyHealth;
+    public bool IsDead;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,13 +13,22 @@ public class Dummy : MonoBehaviour
         m_dummyHealth = m_maxHealth;
     }
 
+    public void ResetDummy()
+    {
+        IsDead = false;
+        m_dummyHealth = m_maxHealth;
+    }
+
     public void RecieveDamage(int _damage)
     {
         m_dummyHealth -= _damage;
 
+        Debug.Log("Dummy has taken Damage HP Left is " + m_dummyHealth);
+
         if (m_dummyHealth <= 0f)
         {
-            Destroy(this);
+            IsDead = true;
+            Debug.Log("Dummy has been killed");
         }
     }
 
